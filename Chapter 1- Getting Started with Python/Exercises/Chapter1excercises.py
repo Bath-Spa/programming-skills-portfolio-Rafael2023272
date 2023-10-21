a7a3a0c693386568939f839db622647dee7f541c
Rafael2023272 <145032666+Rafael2023272@users.noreply.github.com>
2023-10-21 14:28:44 +0400

    Chapter 1 Exercises

---
+++ Chapter 1- Getting Started with Python/Exercises/Chapter1excercises.py
@@ -0,0 +1,45 @@
+#Exercise 1: Print strings
+#Write a Python program to print the following string in a specific format
+
+#Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are
+
+print("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are")
+
+
+#Exercise 2: Write a Python program to get the Python version you are using.
+
+import sys
+print(sys.version)
+
+import platform
+print(platform.python_version())
+
+
+#Exercise 3: Print Date and Time
+#Write a Python program to display the current date and time.
+
+date="October_21_2023"
+print(date)
+
+time="2:15_pm"
+print(time)
+
+
+#Exercise 4: Strings Concatination
+#Write three strings in different variables and print the output as one string. 
+ 
+R="rafael"
+S="sicat"
+P="purisima"
+
+print (R,"" + S,"" + P)
+
+
+#Exercise 5: Compute area of Circle
+#Write a Python program which accepts the radius of a circle from the user and compute the area.
+
+PI = 3.14
+radius = float(input("Enter the radius of a circle: "))
+area = PI * radius * radius
+print("The area of the circle with radius", radius, "is:", area)
+
---
+++ Programming Practice/program1.py
@@ -0,0 +1,42 @@
+#this program is for PRINT FUNCTION
+print("This is text")
+print(10/3)
+print(2 ** 5)
+
+#This program is for CREATING VARIABLE and PRINTING it
+age=21
+print("you are", age, "years old")
+
+#This program is for CONCATINATION
+a="hello"
+b="big"
+c="world"
+print(a,"" + b,"" + c)
+
+#This program is for REPITITION
+a="hello"
+print(a*3)
+
+#This program is for CONCATINATION
+message='hello '+'world'
+print(message)
+
+#This program is for IMPLICIT STRING LITERAL CONCATINATION
+print('Enter the amountof '+
+'sales for each day and '+
+'press Enter.')
+
+#This program is for IMPLICIT STRING LITERAL CONCATINATION
+my_str='one''two''three'
+print(my_str)
+
+
+#This program adds two numbers
+num1=1.5
+num2=6.3
+
+#add two numbers
+sum=num1+num2
+
+print ('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
+
