f0c4f10c7dabcfc21a8a6874093bb9338d6bcb97
Rafael2023272 <145032666+Rafael2023272@users.noreply.github.com>
2023-10-26 18:36:27 +0400

    Update program1.py

--- Programming Practice/program1.py
+++ Programming Practice/program1.py
@@ -40,3 +40,9 @@ sum=num1+num2
 
 print ('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
 
+
+#Program 5
+#python program to assign a message to a variable
+msg="Hello,this is assigning a message to a variable program"
+print(msg)
+
