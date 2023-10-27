6681397060028275abe500a89602d0daf8f32fa0
Rafael2023272 <145032666+Rafael2023272@users.noreply.github.com>
2023-10-27 14:48:25 +0400

    chapter 2 exercises

---
+++ Chapter 2- Variables & Comments/Exercises/Chapter 2Exercises.py
@@ -0,0 +1,42 @@
+#EXERCISE 1 VARIABLES
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+A = "Hello, My name is Rafael Purisima"
+print(A)
+A = "Hello, How are you?"
+print(A)
+
+
+#EXERCISE 2 VARIABLES
+#Find a quote from a famous person you admire. Print the quote and the name of its author.
+
+A="Albert Einstein"
+B="once said,"
+C='"You never fail until you stop trying"'
+
+print(A,""+ B,""+C,)
+
+
+#EXERCISE 3 STRIPPING
+
+W="\trafael purisima\n"
+print(W)
+print(W.lstrip())
+print(W.rstrip())
+print(W.strip())
+
+
+#EXERCISE 4 FAVORITE NUMBER
+
+M=123
+M=(input("Enter your favorite number = "))
+print("My favorite number is = {0}" .format(M))
+
+
+#EXERCISE 5 USB CHOPPER
+
+usb_bought=("total usb bought are=")
+money_left=("total money that is left=")
+print(usb_bought+str(int(50/6)))
+print(money_left+str(50%6))
+
--- Programming Practice/program1.py
+++ Programming Practice/program1.py
@@ -46,3 +46,38 @@ print ('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
 msg="Hello,this is assigning a message to a variable program"
 print(msg)
 
+#Program 7
+#python code to demonstrate escape character
+#string
+
+a="This\nis\tPython Language"
+print(a)
+
+#Program 8
+#Python code to print number
+
+a=int(input("Enter your favorite number = "))
+print("My favorite number is = {0}" .format(a))
+
+#Program 7
+#Python code using stripping
+#string
+
+a="\trafael purisima\n"
+print(a)
+print(a.lstrip())
+print(a.rstrip())
+print(a.strip())
+
+#Program 9
+#python code that calculates how many candies he can buy and how many dollars he will have left
+
+candies_bought=("total candies bought are =")
+money_left="total money that is left ="
+print(candies_bought+str(int(20/2)))
+print(money_left+str(20%2))
+
+
+
+
+
