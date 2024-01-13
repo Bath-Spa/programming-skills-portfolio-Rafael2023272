96e32a45aa9dd1369e666434c0a7fe1e5624bd42
Rafael2023272 <145032666+Rafael2023272@users.noreply.github.com>
2024-01-13 16:26:00 +0400

    Update Chapter1excercises.py

--- Chapter 1- Getting Started with Python/Exercises/Chapter1excercises.py
+++ Chapter 1- Getting Started with Python/Exercises/Chapter1excercises.py
@@ -9,7 +9,6 @@ print("Twinkle, twinkle, little star, How I wonder what you are")
 
 
 #Exercise 2: Write a Python program to get the Python version you are using.
-
 import sys
 print(sys.version)
 
@@ -19,7 +18,6 @@ print(platform.python_version())
 
 #Exercise 3: Print Date and Time
 #Write a Python program to display the current date and time.
-
 date="October 21, 2023"
 print(date)
 
@@ -29,7 +27,6 @@ print(time)
 
 #Exercise 4: Strings Concatination
 #Write three strings in different variables and print the output as one string. 
- 
 R="rafael"
 S="sicat"
 P="purisima"
@@ -39,7 +36,6 @@ print (R,"" + S,"" + P)
 
 #Exercise 5: Compute area of Circle
 #Write a Python program which accepts the radius of a circle from the user and compute the area.
-
 PI = 3.14
 radius = float(input("Enter the radius of a circle: "))
 area = PI * radius * radius
